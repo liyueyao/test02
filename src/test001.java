e67ef2531af45a2b14308a44c3d8523772fd9ef8
liyueyao <975295916@qq.com>
2022-08-01 22:10:01 +0800

    test code

--- src/test001.java
+++ src/test001.java
@@ -1,5 +1,6 @@
 public class test001 {
     public static void main(String[] args){
-        System.out.println("hello world!!");
+        System.out.println("hello world!! 1 ");
+        System.out.println("hello world!! 2 ");
     }
 }
